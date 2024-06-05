cc20fcc3bf1544f0fe8f170dc9af06d20c14a5a9
AngelicaEps <348821562@gapps.yrdsb.ca>
2024-06-05 13:31:15 -0400

    added a background photo

--- Angelica-Final-Project/LoseScreen.java
+++ Angelica-Final-Project/LoseScreen.java
@@ -6,13 +6,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class LoseScreen extends MyWorld
+public class LoseScreen extends World
 {
-
-    /**
-     * Constructor for objects of class LoseScreen.
-     * 
-     */
     Label loseLabel;
     /**
      * Constructor for objects of class LoseScreen.
@@ -20,7 +15,8 @@ public class LoseScreen extends MyWorld
      */
     public LoseScreen()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels. 
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
         loseLabel = new Label("Game Over", 120);
         addObject(loseLabel,300, 200);
         prepare();
@@ -44,14 +40,5 @@ public class LoseScreen extends MyWorld
         Label label = new Label("Press Space to Return to Menu", 50);
         addObject(label,275,311);
         label.setLocation(306,298);
-        Label label2 = new Label("Score : ", 50);
-        addObject(label2,182,88);
-        label2.setLocation(185,102);
-        Label label3 = new Label(0, 50);
-        addObject(label3,306,103);
-        label3.setLocation(393,108);
-        label2.setLocation(268,110);
-        label3.setLocation(397,115);
-        label3.setValue(displayScore);
     }
 }
