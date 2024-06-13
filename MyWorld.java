76bc959d5da2b4589aded8ebafbef04ace55d502
AngelicaEps <348821562@gapps.yrdsb.ca>
2024-06-13 09:22:54 -0400

    moved files

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    Label lifeLabel;
+    Label scoreLabel;
+    public int lives = 3;
+    //score int tracks the number of enemies defeated so that multiple waves can be created
+    public int score = 0;
+    public int displayScore = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +21,39 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        
+        super(600, 400, 1); 
+        Player player = new Player();
+        addObject(player, 300, 300);
+        //adds label for the amount of lives left
+        lifeLabel = new Label(0, 80);
+        addObject(lifeLabel, 50, 50);
+        lifeLabel.setValue(lives);
+        //adds a label for the score
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 550, 50);
+        scoreLabel.setValue(displayScore);
+        //allows player to shoot
+        if(Greenfoot.isKeyDown("Space"))
+        {
+            Attack attack = new Attack();
+            addObject(attack, player.getX()+1, player.getY()-1);
+        }
+    }
+    //spawns in a new enemy at a random location
+    public void newEnemy()
+    {
+        Enemy enemy = new Enemy();
+        int x = Greenfoot.getRandomNumber(550);
+        if(x < 50)
+        {
+            x +=50;
+        }
+        int y = Greenfoot.getRandomNumber(200);
+        if(y < 20)
+        {
+            y+=20;
+        }
+        addObject(enemy,x, y);
     }
 }
